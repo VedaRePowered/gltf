a66e5fd587e5695b88d33fa35c2881617c492107
Ashley Ruglys <ashley.ruglys@gmail.com>
2022-08-08 13:34:36 +0200

    Add texture transform to the normal texture

--- src/material.rs
+++ src/material.rs
@@ -1,4 +1,7 @@
-use crate::{texture, Document};
+use crate::{
+    texture::{self, TextureTransform},
+    Document,
+};
 
 pub use json::material::AlphaMode;
 
@@ -579,6 +582,18 @@ impl<'a, E: json::CustomExtensions> NormalTexture<'a, E> {
     /// Returns the referenced texture.
     pub fn texture(&self) -> texture::Texture<'a, E> {
         self.texture.clone()
+    }
+
+    /// Returns texture transform information
+    #[cfg(feature = "KHR_texture_transform")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "KHR_texture_transform")))]
+    pub fn texture_transform(&self) -> Option<TextureTransform<'a>> {
+        self.json
+            .extensions
+            .as_ref()?
+            .texture_transform
+            .as_ref()
+            .map(TextureTransform::new)
     }
 
     /// Optional application specific data.
